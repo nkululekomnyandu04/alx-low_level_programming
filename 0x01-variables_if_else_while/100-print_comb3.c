232bee7ec4eeae7cfd1cc5ca30d2a17e62dbbbd9
Nkululeko Mnyandu <nkululeko.mnyandu@moov.life>
2023-09-11 16:36:55 -0700

    print combo 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Return: 0
+  */
+int main(void)
+{
+	int i = 48;
+	int j;
+	int cal = 44;
+	int sp = 32;
+
+	for (; i <= 57; i++)
+	{
+		j = i + 1;
+		for (; j <= 57; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 56 || (i == 56 && j != 57))
+			{
+				putchar(cal);
+				putchar(sp);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
