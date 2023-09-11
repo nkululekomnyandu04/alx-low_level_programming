841b6328a624f35215ee5498d98bf3edc76e854a
Nkululeko Mnyandu <nkululeko.mnyandu@moov.life>
2023-09-10 18:26:33 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+  * main - Entry point
+  * Return: 0
+  */
+int main(void)
+{
+	write(3, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 65);
+	return (1);
+}
