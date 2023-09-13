ccaec6f8c391306bba1ea6a3e683f74c10295bc3
Nkululeko Mnyandu <nkululeko.mnyandu@moov.life>
2023-09-13 13:48:09 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * print_sign - Prints the sign of a number
+  * @n: Number to check.
+  * Return: 1 for positive, -1 for negative, 0 if it's zero
+  */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
