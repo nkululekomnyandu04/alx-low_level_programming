fd7a999df7ee5d88bf87856d211287b639e95285
Nkululeko Mnyandu <nkululeko.mnyandu@moov.life>
2023-09-14 16:48:34 -0700

    Mains

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -11,10 +11,8 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
+	int n = 0;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
