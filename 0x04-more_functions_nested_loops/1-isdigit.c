457d3fad9d19c0d2755b8b10636c318648a8391f
Nkululeko Mnyandu <nkululeko.mnyandu@moov.life>
2023-09-15 16:44:20 -0700

    check for a digit (0 through 9)

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * _isdigit -  checks for a digit (0 through 9)
+  * @c: Checked Character
+  * Return: Returns 1 if c is a digit, Returns 0 otherwise
+  */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
