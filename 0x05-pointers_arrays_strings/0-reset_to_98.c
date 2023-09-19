db44b7f6b2e081e10509bff882ee1758ca508417
Nkululeko Mnyandu <nkululeko.mnyandu@moov.life>
2023-09-18 19:39:02 -0700

    Rest to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - pointer to an int and updates the value to 98
+  * @n: Value to update.
+  * Return: 0
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
