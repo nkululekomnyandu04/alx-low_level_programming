9ac51cbbc18cdaabb2f460362e26ae1af3e3880e
Nkululeko Mnyandu <nkululeko.mnyandu@moov.life>
2023-09-19 13:01:37 -0700

    reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * rev_string - function that reverses a string.
+  * @s: Pointer
+  * Return: void
+  */
+
+void rev_string(char *s)
+{
+	int length, a, half;
+	char temp;
+
+	for (length = 0; s[length] != '\0'; length++)
+	;
+	a = 0;
+	half = length / 2;
+
+	while (half--)
+	{
+		temp = s[length - a - 1];
+		s[length - a - 1] = s[a];
+		s[a] = temp;
+		a++;
+	}
+}
