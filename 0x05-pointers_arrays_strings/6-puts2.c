edfd832cca9392c455e7d49fb03a39168cdb1c54
Nkululeko Mnyandu <nkululeko.mnyandu@moov.life>
2023-09-19 19:49:28 -0700

    Puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * puts2 - function that prints every other character of a string.
+  * @str: Pointer
+  * Return: void
+  */
+
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		if (a % 2 == 0)
+		{
+			_putchar(str[a]);
+		}
+		a++;
+	}
+	_putchar('\n');
+}
